d52d716bc9fe76ae1e765f86f8409056a42187e5
Kusum Ijari <kusumijari@gmail.com>
2017-11-09 20:15:09 -0700

    Drag and drop functionality

--- app/assets/javascripts/drinks.js
+++ app/assets/javascripts/drinks.js
@@ -1,11 +1,29 @@
 $( function() {
-    $( "#dragMe" ).draggable();
-    $( "#dropHere" ).droppable({
+    $( ".dragMe" ).draggable();
+    $( ".dropHere" ).droppable({
       drop: function( event, ui ) {
-        $( this )
+          console.log("Dropped in ", $(this).attr("id"));
+          var droppableId = $(this).attr("id");
+          var url=window.location.href;
+          
+          if(url.search("bucket") == -1) {
+            var separator = (url.indexOf("?")===-1)?"?":"&";
+            var newParam= separator + "bucket="+droppableId;
+            var newUrl=url.replace(newParam,"");
+            newUrl = newUrl + newParam;
+          }
+          else {
+            console.log("Repeated")
+            var newUrl = url.replace(/[\?|&]bucket(.*)/, "");
+            console.log("New url", newUrl);
+            var separator = (newUrl.indexOf("?")===-1)?"?":"&";
+            var newParam= separator + "bucket="+droppableId;
+            newUrl = newUrl + newParam;
+          }
+          window.history.pushState("", "", newUrl) ;
+        $(this)
+          // .append(ui.draggable.css({position: 'relative', width: '100%', margin: '0 auto'}))
           .addClass( "ui-state-highlight" )
-          .find( "p" )
-            .html( "Dropped!" );
-      }
+    }
     });
   } );
